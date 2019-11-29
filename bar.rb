a8855e4632597bc1498ae01dcd6894af9a056f3e
Patrick O'Neill <paddyjoneill@hotmail.com>
2019-11-29 22:01:17 +0000

    working on bar

--- bar.rb
+++ bar.rb
@@ -1,3 +1,24 @@
 class Bar
 
+  attr_reader :name, :stock
+
+  def initialize(name)
+    @name = name
+    @stock = Hash.new(0)
+  end
+
+  def add_drink(drink)
+    if @stock.include?(drink)
+      @stock[drink] += 1
+    else
+      @stock[drink] = 1
+    end
+  end
+
+  def remove_drink(drink)
+    if @stock.include?(drink) && @stock[drink] >0
+      @stock[drink] -= 1 
+    end
+  end
+
 end
--- specs/bar_spec.rb
+++ specs/bar_spec.rb
@@ -9,4 +9,28 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestBar < MiniTest::Test
 
+  def setup
+    @beer = Drink.new("Beer", 4)
+    @redwine = Drink.new("Red Wine", 6)
+    @whitewine = Drink.new("White Wine", 6)
+    @mainbar = Bar.new("Main Bar")
+  end
+
+  def test_bar_has_name
+    assert_equal("Main Bar", @mainbar.name)
+  end
+
+  def test_add_drink_to_bar
+    @mainbar.add_drink(@beer)
+    assert_equal(1, @mainbar.stock[@beer])
+  end
+
+  def test_remove_drink_from_bar
+    @mainbar.add_drink(@beer)
+    @mainbar.add_drink(@beer)
+    assert_equal(2, @mainbar.stock[@beer])
+    @mainbar.remove_drink(@beer)
+    assert_equal(1, @mainbar.stock[@beer])
+  end
+
 end
